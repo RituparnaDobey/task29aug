12bd2ecf85bc2f720354ec91c7d359578254e256
RituparnaDobey <rituparnadobey4@gmail.com>
2023-08-29 20:05:26 +0200

    file added

---
+++ task.py
@@ -0,0 +1,123 @@
+# Task 2 - get the difference
+
+# for i in range(3):    
+
+
+#     first_num = float(input('Please  enter the 1st number: '))
+#     second_num = float(input('Please enter the 2nd number: '))
+
+#     if first_num > second_num:
+#         difference = (first_num - second_num) * 2
+#         print('The result of the calculation is:',difference)
+
+#     elif first_num == second_num:
+#         print('The result of the difference is: 0')
+
+#     else:
+#         first_num - second_num
+#         difference = abs(first_num - second_num)
+#         print('The result of the difference is:', difference)
+
+
+# Task 3 - odd or even number
+
+# num = int(input('Please enter a number: '))
+
+# if num % 2 == 0:
+#     print('This is an even number')
+# else:
+#     print('This is an odd number')
+
+# Task 4 - circle area
+
+# import math
+
+# radius = float(input("Input the radius of the circle: "))
+
+# area = math.pi * radius ** 2
+# area = round(area, 2)  
+
+# print(f"The area of the circle with radius {radius} is: {area}")
+
+
+# Task 5 - guess a number
+
+# number_to_guess = 7
+
+# while True:
+
+#     user_guess = int(input("Guess a number between 1 and 10: "))
+
+#     if user_guess == number_to_guess:
+#         print("Well guessed!")
+#         break 
+#     else:
+#         print("Try again!")
+
+
+# x = int(input("First number: "))
+# y = int(input("Second number: "))
+
+# result = x + y
+
+# if result > 15 and result < 20:
+#     result = 20
+#     print("Calculated sum is ", result)
+
+# else:
+#     print("Calculated sum is ", result)
+
+
+# a = input("First value: ")
+# b = input("Second value: ")
+
+# print("Before swapping: a =", a, " b =", b)
+
+# temp = a
+# a = b
+# b = temp
+
+# print("After swapping: a =", a, " b =" b)
+
+
+# x = float(input("First number: "))
+# y = float(input("Second number: "))
+# z = float(input("Third number: "))
+
+# print("The maximum value is ", max(x, y ,z))
+# print("The minimum value is ", min(x, y ,z))
+
+
+# x = input("Type your value: ")
+
+# if x == "0":
+#     x = False
+# elif x == "1":
+#     x = True
+# else:
+#     pass
+
+
+# print("Your entered value is now ", x)
+
+
+
+# x = int(input("First number: "))
+# y = int(input("Second number: "))
+
+# if x % y == 0:
+#     print("First number is divisible by second number, result =", x / y)
+# elif y % x == 0:
+#     print("Second number is divisible by first number, result =", y / x)
+# else:
+#     print("Numbers are non-divisable!")
+
+
+
+
+
+
+    
+
+
+
